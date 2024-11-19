ad5f78d9371ba53868ebb004741c5817134c7107
Tom D Bebbington <tombebb@protonmail.com>
2024-11-19 17:30:21 +0000

    Basic library view

--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,14 @@
 export interface GameData {
+    libraryType: string
+    libraryId: string
+    genres: string[]
+    description: string
     name: string
+    installStatus: GameInstallStatus
+}
+export enum GameInstallStatus {
+    Installed,
+    Installing,
+    InLibrary,
+    Queued,
 }
--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -1,3 +1,55 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { GameData, GameInstallStatus } from "../models"
+
+type View = "grid" | "table" | "list"
+const games = ref<GameData[]>([
+    {
+        name: "Atelier Sophie 2",
+        libraryId: "1621310",
+        libraryType: "steam",
+        description: "Play as sophie 2",
+        genres: ["JRPG", "Turn-Based"],
+        installStatus: GameInstallStatus.Installed,
+    },
+])
+const views: { name: string; value: View }[] = [
+    { name: "Grid View", value: "grid" },
+    { name: "Table View", value: "table" },
+    { name: "List View", value: "list" },
+]
+const view = ref<View>("list")
+</script>
 <template>
+    <select class="select" v-model="view">
+        <option v-for="view in views" :value="view.value">
+            {{ view.name }}
+        </option>
+    </select>
+    <template v-if="view === 'table'">
+        <table class="table table-zebra select-none">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Genres</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="game in games">
+                    <td>{{ game.name }}</td>
+                    <td>{{ game.description }}</td>
+                    <td class="flex gap-2">
+                        <div
+                            v-for="genre in game.genres"
+                            class="badge badge-neutral"
+                        >
+                            {{ genre }}
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </template>
     <div>Library!</div>
 </template>
