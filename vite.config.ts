1d9f7ba5a6ab3a2cf1f23763cb8758e0c2fb1c87
Tom D Bebbington <tombebb@protonmail.com>
2024-12-02 17:14:41 +0000

    Fix frontend build

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ const host = process.env.TAURI_DEV_HOST
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
     plugins: [solidPlugin()],
-
+    build: {
+        target: "esnext",
+    },
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
     // 1. prevent vite from obscuring rust errors
