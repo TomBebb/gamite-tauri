5fb27e20d7992b75283e208907927fe48045d7cb
Tom D Bebbington <tombebb@protonmail.com>
2024-12-04 13:35:15 +0000

    Simplify app structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 import "./index.css"
-import NavMenu from "./components/NavMenu.vue"
 import Titlebar from "./components/Titlebar.vue"
+import NavMenu from "./components/NavMenu.vue"
 </script>
 <template>
-    <Titlebar class="sticky top-0 z-[100]" />
-    <div class="drawer drawer-open">
-        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content flex flex-col p-5">
-            <RouterView />
-        </div>
-        <div class="drawer-side flex w-56 flex-row bg-base-200">
-            <NavMenu class="flex-1" />
-        </div>
-    </div>
+    <Titlebar />
+    <NavMenu />
+    <RouterView />
 </template>
