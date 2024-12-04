f0a1d3aeff84323c62f790dd082fd2de87d958e5
Tom D Bebbington <tombebb@protonmail.com>
2024-12-04 11:17:20 +0000

    Use router-link

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import { routes } from "../router"
+import router, { routes } from "../router"
+import { RouteRecordSingleView } from "vue-router"
+
+function onClick(route: RouteRecordSingleView, ev: Event) {
+    ev.preventDefault()
+    router.push(route.path)
+}
 </script>
 <template>
     <v-navigation-drawer>
         <v-list-item
-            link
             v-for="route in routes"
             :title="route.name"
             :href="route.path"
+            @click="onClick(route, $event)"
         />
     </v-navigation-drawer>
 </template>
