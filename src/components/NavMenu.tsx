56cbeae6f431686917ae95738ecb68900af16a32
Tom D Bebbington <tombebb@protonmail.com>
2025-05-19 14:32:50 +0100

    Fix nav

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,19 +1,22 @@
 import { routes } from "../router"
-import { For } from "solid-js"
+import { createEffect, For } from "solid-js"
 import { Icon } from "@iconify-icon/solid"
 import { A, useLocation } from "@solidjs/router"
 
 export default function () {
     const location = useLocation()
+    createEffect(() => {
+        console.log("location", location.pathname)
+    })
     return (
-        <ul class="menu menu-md w-56 rounded-box bg-base-200">
+        <ul class="menu menu-md rounded-box bg-base-200 w-56">
             <For each={routes}>
                 {(route) => (
                     <li>
                         <A
                             href={route.path}
                             classList={{
-                                active: route.path === location.pathname,
+                                "menu-active": route.path === location.pathname,
                             }}
                         >
                             <div class="flex flex-row items-center justify-start gap-2">
