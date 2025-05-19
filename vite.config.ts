10e778260f5a20f7047291efdad8364d2d449bda
Tom D Bebbington <tombebb@protonmail.com>
2025-05-19 14:26:54 +0100

    Update tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite"
 import solidPlugin from "vite-plugin-solid"
-
+import tailwindcss from "@tailwindcss/vite"
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-    plugins: [solidPlugin()],
+    plugins: [solidPlugin(), tailwindcss()],
     build: {
         target: "esnext",
     },
