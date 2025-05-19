8d41199e706aef26630436d1494264013953a358
Tom D Bebbington <tombebb@protonmail.com>
2025-05-19 14:05:13 +0100

    Fix nav

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,22 +1,26 @@
 import { routes } from "../router"
 import { For } from "solid-js"
 import { Icon } from "@iconify-icon/solid"
+import { A, useLocation } from "@solidjs/router"
 
 export default function () {
+    const location = useLocation()
     return (
         <ul class="menu menu-md w-56 rounded-box bg-base-200">
             <For each={routes}>
                 {(route) => (
                     <li>
-                        <a
+                        <A
                             href={route.path}
                             classList={{
-                                active: true,
+                                active: route.path === location.pathname,
                             }}
                         >
-                            <Icon icon={route.icon} />
-                            {route.name}
-                        </a>
+                            <div class="flex flex-row items-center justify-start gap-2">
+                                <Icon icon={route.icon} />
+                                {route.name}
+                            </div>
+                        </A>
                     </li>
                 )}
             </For>
