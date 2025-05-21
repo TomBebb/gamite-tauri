a426c2f7e5b41db69c0072c1134b16abced37efa
Tom D Bebbington <tombebb@protonmail.com>
2025-05-21 17:03:39 +0100

    Fix init.sql typo

--- src-tauri/src/db/init.sql
+++ src-tauri/src/db/init.sql
@@ -1,33 +1,36 @@
 BEGIN TRANSACTION;
 CREATE TABLE
-    IF NOT EXISTS "games" (
-                              "id" INTEGER NOT NULL,
-                              "name" BLOB NOT NULL,
-                              "library_id" TEXT NOT NULL,
-                              "library_type" TEXT NOT NULL,
-                              "install_status" INT NOT NULL,sTORE
-                              "description" TEXT,
-                              "play_time_secs" INTEGER NOT NULL,
-                              "release_date" TIMESTAMP,
-                              "last_played" TIMESTAMP,
-                              PRIMARY KEY ("id" AUTOINCREMENT)
-    );
+    IF NOT EXISTS "games"
+(
+    "id"             INTEGER NOT NULL,
+    "name"           TEXT    NOT NULL,
+    "library_id"     TEXT    NOT NULL,
+    "library_type"   TEXT    NOT NULL,
+    "install_status" INT     NOT NULL,
+    "description"    TEXT,
+    "play_time_secs" INTEGER NOT NULL,
+    "release_date"   TIMESTAMP,
+    "last_played"    TIMESTAMP,
+    PRIMARY KEY ("id" AUTOINCREMENT)
+);
 
 CREATE TABLE
-    IF NOT EXISTS "genres" (
-                               "id" INTEGER NOT NULL,
-                               "name" TEXT NOT NULL,
-                               "metadata_source" TEXT NOT NULL,
-                               "metadata_id" TEXT NOT NULL,
-                               PRIMARY KEY ("id" AUTOINCREMENT)
-    );
+    IF NOT EXISTS "genres"
+(
+    "id"              INTEGER NOT NULL,
+    "name"            TEXT    NOT NULL,
+    "metadata_source" TEXT    NOT NULL,
+    "metadata_id"     TEXT    NOT NULL,
+    PRIMARY KEY ("id" AUTOINCREMENT)
+);
 
 CREATE TABLE
-    IF NOT EXISTS "game_genres" (
-                                    "game_id" INTEGER NOT NULL,
-                                    "genre_id" INTEGER NOT NULL,
-                                    PRIMARY KEY ("game_id", "genre_id")
-    );
+    IF NOT EXISTS "game_genres"
+(
+    "game_id"  INTEGER NOT NULL,
+    "genre_id" INTEGER NOT NULL,
+    PRIMARY KEY ("game_id", "genre_id")
+);
 
 CREATE UNIQUE INDEX IF NOT EXISTS "genres_metadata" ON "genres" ("metadata_source", "metadata_id");
 
