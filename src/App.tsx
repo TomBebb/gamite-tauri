be4cd647f88b9151a34e525182c0f979070358b5
Tom D Bebbington <tombebb@protonmail.com>
2025-05-22 15:20:08 +0100

    Fix titlebar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { RouteSectionProps } from "@solidjs/router"
 export default function ({ children }: RouteSectionProps) {
     return (
         <div class="flex min-h-screen flex-col">
-            <Titlebar class="show-on-desktop sticky top-0" />
+            <Titlebar class="show-on-desktop sticky top-0 z-50" />
             <div class="flex h-screen flex-grow flex-row">
                 <NavMenu class="min-w-20px flex-1" />
                 <div class="flex-5">{children}</div>
