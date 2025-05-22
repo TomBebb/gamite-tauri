6d0ad7764729d724ed6b172c99a663691b741f57
Tom D Bebbington <tombebb@protonmail.com>
2025-05-22 15:15:48 +0100

    Make navbar take up full height

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import "./index.css"
 import NavMenu from "./components/NavMenu.js"
 import Titlebar from "./components/Titlebar"
 import { RouteSectionProps } from "@solidjs/router"
-import { isDesktop } from "./common/bigScreen"
-import { Show } from "solid-js"
 
 export default function ({ children }: RouteSectionProps) {
     return (
-        <div class="flex flex-col">
-            <Show when={isDesktop()}>
-                <Titlebar class="sticky top-0" />
-            </Show>
-            <div class="flex flex-row">
-                <NavMenu class="min-w-20px flex-1" />
+        <div class="flex min-h-screen flex-col">
+            <Titlebar class="show-on-desktop sticky top-0" />
+            <div class="flex flex-grow flex-row">
+                <NavMenu class="min-w-20px h-screen flex-1" />
                 <div class="flex-5">{children}</div>
             </div>
         </div>
