c980babaa3cdbbd189989b35053fce492f574002
Tom D Bebbington <tombebb@protonmail.com>
2025-05-22 15:23:16 +0100

    Scroll in page only

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import { RouteSectionProps } from "@solidjs/router"
 
 export default function ({ children }: RouteSectionProps) {
     return (
-        <div class="flex min-h-screen flex-col">
+        <div class="h-screen flex-col overflow-hidden">
             <Titlebar class="show-on-desktop sticky top-0 z-50" />
             <div class="flex h-screen flex-grow flex-row">
                 <NavMenu class="min-w-20px flex-1" />
-                <div class="flex-5">{children}</div>
+                <div class="flex-5 overflow-scroll">{children}</div>
             </div>
         </div>
     )
