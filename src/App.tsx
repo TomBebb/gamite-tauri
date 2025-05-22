b55b767c315311883f8c0b5d1eeba97a62dc6d67
Tom D Bebbington <tombebb@protonmail.com>
2025-05-22 15:17:29 +0100

    Simplify full height

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ export default function ({ children }: RouteSectionProps) {
     return (
         <div class="flex min-h-screen flex-col">
             <Titlebar class="show-on-desktop sticky top-0" />
-            <div class="flex flex-grow flex-row">
-                <NavMenu class="min-w-20px h-screen flex-1" />
+            <div class="flex h-screen flex-grow flex-row">
+                <NavMenu class="min-w-20px flex-1" />
                 <div class="flex-5">{children}</div>
             </div>
         </div>
